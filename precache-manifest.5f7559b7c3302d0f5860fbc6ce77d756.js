4d4955d011d9357869c5ebefffe93ffc7a40af4a
unknown <leejiwon8433@gmail.com>
2020-07-16 04:49:50 +0900

    Updates

---
+++ precache-manifest.5f7559b7c3302d0f5860fbc6ce77d756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e14c13f7eee06ad2560a4debf2b397",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4aac46307c60d1cd42e0",
+    "url": "/movie_app_2020/static/css/main.605d1d63.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aac46307c60d1cd42e0",
+    "url": "/movie_app_2020/static/js/main.a89a548e.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
